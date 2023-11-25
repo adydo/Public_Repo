61d6b4627b2976d39367e1ade8e69aa04502901c
adydo <116827345+adydo@users.noreply.github.com>
2023-11-25 13:14:04 +0100

    Add files via upload

---
+++ GetLocationFromIPs.ps1
@@ -0,0 +1,32 @@
+﻿
+param(
+    [Parameter()] 
+    [String]$path
+)
+
+#$path = "c:\temp\test.csv"
+
+$sito = "https://api.country.is/"
+$geo_track = @()
+
+
+Import-Csv $path | Foreach-Object { 
+
+     $ip = $_.PSObject.Properties.Value
+     $uri = $sito + $ip
+     Start-Sleep -m 500
+try{
+     $response = Invoke-RestMethod $uri 
+}
+catch{
+    $response.ip = $ip
+    $response.country = 'Not Available' 
+}
+
+     $geo_track += [PSCustomObject]@{
+            IP = $response.ip
+            Country = $response.country
+     }
+}
+
+$geo_track | Format-Table
